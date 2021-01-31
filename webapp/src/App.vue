175064c2fb7f1a501727cf4801d4df9ab6225c2a
SLL288 <shaolun28@gmail.com>
2021-01-31 12:43:04 -0800

    vue update

---
+++ webapp/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Posts v-bind:Posts="PostsMessage"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Posts from './components/Posts.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Posts
+  },
+  data(){
+    return {
+      PostsMessage:[
+        {id:1,
+          title:"news1",
+          content:"content1" 
+        },
+        {id:2,
+          title:"news2",
+          content:"content2" 
+        },
+        {id:3,
+          title:"news3",
+          content:"content3" 
+        },
+        {id:4,
+          title:"news4",
+          content:"content4" 
+        },
+        {id:5,
+          title:"news5",
+          content:"content5" 
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+
+@import './html5_template/assets/css/main.css'
+</style>
---
+++ webapp/src/components/Posts.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="Posts">
+        <div  v-bind:key="elements.id" v-for="elements in Posts">
+            <h2>{{elements.title}}</h2>
+            <h6>{{elements.content}}</h6>            
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'Posts',
+  props: ["Posts"]
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+@import '../html5_template/assets/css/main.css'
+
+</style>
